81537c9b03d7af04f34cb2bef880adfcec8ee819
Andrea Barone <101096469+bandrea01@users.noreply.github.com>
2026-01-30 16:36:24 +0100

    feat: add validation to AdminController

--- src/main/java/it/unisalento/music_virus_project/event_fundraising_service/controllers/AdminController.java
+++ src/main/java/it/unisalento/music_virus_project/event_fundraising_service/controllers/AdminController.java
@@ -5,12 +5,14 @@ import it.unisalento.music_virus_project.event_fundraising_service.service.imple
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/event-fundraising/admin")
+@Validated
 public class AdminController {
     private final AdminService adminService;
 
